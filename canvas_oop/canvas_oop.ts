457addb01f0b39f14227de7484daacfc71072d87
oznightray <oznightray@163.com>
2016-03-07 01:36:23 +0800

    test1

--- canvas_oop/canvas_oop.ts
+++ canvas_oop/canvas_oop.ts
@@ -61,9 +61,12 @@ class Rect extends DisplayObject {
 class TextField extends DisplayObject {
 
     render(context: CanvasRenderingContext2D) {
-        context.font = "20px Arial";
-        context.fillStyle = '#000000';
-        context.fillText('HelloWorld', 0, 20);
+        context.font = "11px Arial";
+        context.fillStyle = '#FFFFFF';
+        context.fillText('osu!droid by Pesets&neico', 0, 230);
+        context.fillText('osu! is @ peppy 2007-2015', 0, 243);
+         context.fillText('Performance Ranking', 290, 230);
+         context.fillText('Provided by iBancho', 292, 243);
     }
 }
 
@@ -104,29 +107,56 @@ var context = canvas.getContext("2d");
 
 
 var rect = new Rect();
-rect.width = 200;
-rect.height = 100;
-rect.color = '#00FF00'
+rect.width = 410;
+rect.height = 250;
+rect.color = '#0000FF'//1C86EE'
 
 
 var rect2 = new Rect();
-rect2.width = 300;
-rect2.height = 50;
-rect2.x = 200;
-rect2.y = 200;
-rect2.rotation = Math.PI / 8;
-rect2.color = '#00FFFF'
+rect2.width = 100;
+rect2.height = 20;
+rect2.x = 0;
+rect2.y = 120;
+rect2.rotation = Math.PI / 10;
+rect2.color = '#FFFF00'
+
+var rect3 = new Rect();
+rect3.width = 100;
+rect3.height =20;
+
+//rect3.rotation = Math.PI / 8;
+rect3.x = 385;
+rect3.y = 130;
+rect3.color = '#FFFF00'
 
 var text = new TextField();
-text.x = 10;
+text.x = 5;
 
-var bitmap = new Bitmap();
-bitmap.source = '1.png';
+
+var bitmap1 = new Bitmap();
+bitmap1.source = '1.png';
+bitmap1.x = 40;
+bitmap1.y = 20;
+
+var bitmap2 = new Bitmap();
+bitmap2.source = '2.png';
+bitmap2.x = 212;
+bitmap2.y = 50;
+
+var bitmap3 = new Bitmap();
+bitmap3.source = '3.png';
+bitmap3.x = 239;
+bitmap3.y = 100;
+
+var bitmap4 = new Bitmap();
+bitmap4.source = '4.png';
+bitmap4.x = 215;
+bitmap4.y = 150;
 
 //渲染队列
-var renderQueue = [rect, rect2, text,bitmap];
+var renderQueue = [rect, rect2,rect3,text,bitmap1,bitmap2,bitmap3,bitmap4];
 //资源加载列表
-var imageList = ['1.png'];
+var imageList = ['1.png','2.png','3.png','4.png'];
 
 //先加载资源，加载成功之后执行渲染队列
 loadResource(imageList, function() {
