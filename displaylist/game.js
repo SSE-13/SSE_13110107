8d237626ec735ad0f9697f14fc6634428f5da45d
oznightray <oznightray@163.com>
2016-03-28 23:15:48 +0800

    Finish Homework 5

--- displaylist/game.js
+++ displaylist/game.js
@@ -4,19 +4,21 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 var humanContainer = new render.DisplayObjectContainer();
+humanContainer.x = 0;
 var head = new render.Bitmap();
-head.source = "wander-icon.jpg";
-head.y -= 100;
+head.source = "head.png";
+head.y -= 160;
+head.x -= 30;
 humanContainer.addChild(head);
 var renderCore = new render.RenderCore();
-renderCore.start(humanContainer, ["wander-icon.jpg"]);
+renderCore.start(humanContainer, ["head.png", "trunk.png", "left_arm.png", "right_arm.png", "left_leg.png", "right_leg.png"]);
 var HumanBody = (function (_super) {
     __extends(HumanBody, _super);
     function HumanBody() {
         _super.apply(this, arguments);
     }
     HumanBody.prototype.onTicker = function (duringTime) {
-        this.x = 400;
+        this.x += 1;
         this.y = 300;
         this.rotation += 1;
     };
@@ -27,17 +29,31 @@ var rect = new render.Rect();
 rect.x -= 50;
 rect.y -= 50;
 var trunk = new render.Bitmap();
-trunk.source = "wander-icon.jpg";
+trunk.source = "trunk.png";
+trunk.x -= 20;
 trunk.y -= 100;
-var down = new render.Bitmap();
-down.source = "wander-icon.jpg";
-down.y += 200;
-var right = new render.Bitmap();
-down.source = "wander-icon.jpg";
-down.y += 200;
+var leftarm = new render.Bitmap();
+leftarm.source = "left_arm.png";
+leftarm.y -= 210;
+leftarm.x -= 240;
+var rightarm = new render.Bitmap();
+rightarm.source = "right_arm.png";
+rightarm.y -= 90;
+rightarm.x += 20;
+var leftleg = new render.Bitmap();
+leftleg.source = "left_leg.png";
+leftleg.y += 45;
+leftleg.x += 10;
+var rightleg = new render.Bitmap();
+rightleg.source = "right_leg.png";
+rightleg.y += 45;
+rightleg.x -= 30;
 humanContainer.addChild(rect);
-//humanContainer.addChild(trunk);
-//humanContainer.addChild(down);
+humanContainer.addChild(trunk);
+humanContainer.addChild(leftarm);
+humanContainer.addChild(rightarm);
+humanContainer.addChild(leftleg);
+humanContainer.addChild(rightleg);
 var body = new HumanBody(humanContainer);
 ticker.start([body]);
 //# sourceMappingURL=game.js.map
--- displaylist/game.ts
+++ displaylist/game.ts
@@ -4,13 +4,15 @@ module game {
 }
 
 var humanContainer = new render.DisplayObjectContainer();
+humanContainer.x = 0;
 var head = new render.Bitmap();
-head.source = "wander-icon.jpg";
-head.y -=100;
+head.source = "head.png";
+head.y -=160;
+head.x -= 30;
 humanContainer.addChild(head);
 
 var renderCore = new render.RenderCore();
-renderCore.start(humanContainer, ["wander-icon.jpg"]);
+renderCore.start(humanContainer, ["head.png","trunk.png","left_arm.png","right_arm.png","left_leg.png","right_leg.png"]);
 
 
 class HumanBody extends Body {
@@ -18,7 +20,7 @@ class HumanBody extends Body {
 
     onTicker(duringTime: number) {
 
-         this.x = 400 ;
+         this.x += 1;
          this.y = 300;
          this.rotation += 1;
 
@@ -32,17 +34,31 @@ rect.x -= 50;
 rect.y -= 50;
 
 var trunk = new render.Bitmap();
-trunk.source = "wander-icon.jpg";
+trunk.source = "trunk.png";
+trunk.x -=20;
 trunk.y -= 100;
-var down = new render.Bitmap();
-down.source = "wander-icon.jpg";
-down.y += 200;
-var right = new render.Bitmap();
-down.source = "wander-icon.jpg";
-down.y += 200;
-humanContainer.addChild(rect);
-//humanContainer.addChild(trunk);
-//humanContainer.addChild(down);
+var leftarm = new render.Bitmap();
+leftarm.source = "left_arm.png";
+leftarm.y -= 210;
+leftarm.x -=240;
+var rightarm = new render.Bitmap();
+rightarm.source = "right_arm.png";
+rightarm.y -=90;
+rightarm.x +=20;
+var leftleg = new render.Bitmap();
+leftleg.source = "left_leg.png";
+leftleg.y += 45;
+leftleg.x += 10;
+var rightleg = new render.Bitmap();
+rightleg.source = "right_leg.png";
+rightleg.y += 45;
+rightleg.x -=30;
+//humanContainer.addChild(rect);
+humanContainer.addChild(trunk);
+humanContainer.addChild(leftarm);
+humanContainer.addChild(rightarm);
+humanContainer.addChild(leftleg);
+humanContainer.addChild(rightleg);
 
 var body = new HumanBody(humanContainer);
 ticker.start([body]);
